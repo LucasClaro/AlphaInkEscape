70ffec02723ce458992d163b455945ab5fadd621
GustavoYamauchi <gyxgames31@gmail.com>
2019-10-06 22:40:56 -0300

    Commit fase4 add

--- AlhpaInkEscape/AlhpaInkEscape/FaseController.h
+++ AlhpaInkEscape/AlhpaInkEscape/FaseController.h
@@ -7,6 +7,7 @@
 #include <allegro5/allegro_native_dialog.h>
 
 #include "Struct.h"
+#include "Fase4Binary.h"
 //#include "Fase0.h"
 //#include "Fase1.h"
 //#include "Fase2.h"
@@ -20,13 +21,17 @@ void selecionaFase(ALLEGRO_DISPLAY* janela, ALLEGRO_EVENT_QUEUE* fila_eventos, P
 {
 	switch (progresso->proximaSala)
 	{
-	case 0: JogarFase0(janela, fila_eventos, progresso);
+	case 0: 
+		//JogarFase0(janela, fila_eventos, progresso);
 		return;
-	case 1: JogarFase1(janela, fila_eventos, progresso);
+	case 1: //JogarFase1(janela, fila_eventos, progresso);
 		return;
-	case 2: JogarFase2(janela, fila_eventos, progresso);
+	case 2: //JogarFase2(janela, fila_eventos, progresso);
 		return;
-	case 3: JogarFase3(janela, fila_eventos, progresso);
+	case 3: //JogarFase3(janela, fila_eventos, progresso);
+		return;
+	case 4: 
+		JogarFase4Binary(janela, fila_eventos, progresso);
 		return;
 	default: printf("Fase não encontrada"); return;
 		break;
--- AlhpaInkEscape/AlhpaInkEscape/Origem.c
+++ AlhpaInkEscape/AlhpaInkEscape/Origem.c
@@ -67,12 +67,12 @@ int main(void) {
 
 	// Preenche o objeto de progresso
 	Progresso progresso;
-	progresso.proximaSala = 0;
+	progresso.proximaSala = 4;
 	progresso.Gameover = 0;
-	progresso.Sala1 = 0;
-	progresso.Sala2 = 0;
-	progresso.Sala3 = 0;
-	progresso.Item1 = 0;
+	progresso.Salas[0] = 0;
+	progresso.Salas[1] = 0;
+	progresso.Salas[2] = 0;
+	progresso.Salas[3] = 0;
 
 	// Cria o ponteiro para progresso que será mandado para o resto do programa
 	Progresso* prog = &progresso;
