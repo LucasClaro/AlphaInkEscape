9002d242b9f08b583a6a6ab47ffa9f3bdd76222a
Lucas Claro <lucasbrclaro@gmail.com>
2019-10-09 16:38:41 -0300

    movendo as coisas

--- AlhpaInkEscape/AlhpaInkEscape/Fase3Brasil.h
+++ AlhpaInkEscape/AlhpaInkEscape/Fase3Brasil.h
@@ -12,21 +12,23 @@
 
 int JogarFase3Brasil(ALLEGRO_DISPLAY* janela, ALLEGRO_EVENT_QUEUE* fila_eventos, Progresso* prog) {
 
-	Objeto *mapa;
+	
+
+	Objeto *notaOnca;
+	notaOnca = (Objeto*)malloc(sizeof(Objeto));
+	notaOnca->bitmap = NULL;
+	notaOnca->x = 120;
+	notaOnca->y = 100;
+	notaOnca->largura = 230;
+	notaOnca->altura = 200;
+
+	Objeto* mapa;
 	mapa = (Objeto*)malloc(sizeof(Objeto));
 	mapa->bitmap = NULL;
-	mapa->x = LARGURA_TELA/2 - 450;
-	mapa->y = ALTURA_TELA/2 - 450;
-	mapa->largura = 900;
-	mapa->altura = 900;
-
-	Objeto *notaOnça;
-	notaOnça = (Objeto*)malloc(sizeof(Objeto));
-	notaOnça->bitmap = NULL;
-	notaOnça->x = 120;
-	notaOnça->y = 100;
-	notaOnça->largura = 230;
-	notaOnça->altura = 200;
+	mapa->largura = 650;
+	mapa->altura = 650;
+	mapa->x = notaOnca->largura + notaOnca->x + 20;
+	mapa->y = ALTURA_TELA / 2 - 330;
 
 	Objeto* notaTatu;
 	notaTatu = (Objeto*)malloc(sizeof(Objeto));
@@ -52,33 +54,37 @@ int JogarFase3Brasil(ALLEGRO_DISPLAY* janela, ALLEGRO_EVENT_QUEUE* fila_eventos,
 	notaMico->largura = 230;
 	notaMico->altura = 200;
 
-	Objeto marcaOnça;
-	marcaOnça.bitmap = NULL;
-	//mapa.x = LARGURA_TELA - 100;
-	//mapa.y = ALTURA_TELA / 2 - 50;
-	//mapa.largura = 100;
-	//mapa.altura = 100;
+	Objeto* marcaOnca;
+	marcaOnca = (Objeto*)malloc(sizeof(Objeto));
+	marcaOnca->bitmap = NULL;
+	marcaOnca->x = notaOnca->x + notaOnca->largura - 55;
+	marcaOnca->y = notaOnca->y + notaOnca->altura - 55;
+	marcaOnca->largura = 50;
+	marcaOnca->altura = 50;
 
-	Objeto marcaTatu;
-	marcaTatu.bitmap = NULL;
-	//mapa.x = LARGURA_TELA - 100;
-	//mapa.y = ALTURA_TELA / 2 - 50;
-	//mapa.largura = 100;
-	//mapa.altura = 100;
+	Objeto *marcaTatu;
+	marcaTatu = (Objeto*)malloc(sizeof(Objeto));
+	marcaTatu->bitmap = NULL;
+	marcaTatu->x = notaTatu->x + notaTatu->largura - 55;
+	marcaTatu->y = notaTatu->y + notaTatu->altura - 55;
+	marcaTatu->largura = 50;
+	marcaTatu->altura = 50;
 
-	Objeto marcaJacare;
-	marcaJacare.bitmap = NULL;
-	//mapa.x = LARGURA_TELA - 100;
-	//mapa.y = ALTURA_TELA / 2 - 50;
-	//mapa.largura = 100;
-	//mapa.altura = 100;
+	Objeto *marcaJacare;
+	marcaJacare = (Objeto*)malloc(sizeof(Objeto));
+	marcaJacare->bitmap = NULL;
+	marcaJacare->x = notaJacare->x + notaJacare->largura - 55;
+	marcaJacare->y = notaJacare->y + notaJacare->altura - 55;
+	marcaJacare->largura = 50;
+	marcaJacare->altura = 50;
 
-	Objeto marcaMico;
-	marcaMico.bitmap = NULL;
-	//mapa.x = LARGURA_TELA - 100;
-	//mapa.y = ALTURA_TELA / 2 - 50;
-	//mapa.largura = 100;
-	//mapa.altura = 100;
+	Objeto *marcaMico;
+	marcaMico = (Objeto*)malloc(sizeof(Objeto));
+	marcaMico->bitmap = NULL;
+	marcaMico->x = notaMico->x + notaMico->largura - 55;
+	marcaMico->y = notaMico->y + notaMico->altura - 55;
+	marcaMico->largura = 50;
+	marcaMico->altura = 50;
 
 	Objeto saidaDireita;
 	saidaDireita.bitmap = NULL;
@@ -105,22 +111,20 @@ int JogarFase3Brasil(ALLEGRO_DISPLAY* janela, ALLEGRO_EVENT_QUEUE* fila_eventos,
 
 	mapa->bitmap = al_load_bitmap("Imgs/mapabrasil.png");
 
-	notaOnça->bitmap = al_load_bitmap("Imgs/bloco.png");
-	notaTatu->bitmap = al_load_bitmap("Imgs/bloco.png");
-	notaJacare->bitmap = al_load_bitmap("Imgs/bloco.png");
-	notaMico->bitmap = al_load_bitmap("Imgs/bloco.png");
+	notaOnca->bitmap = al_load_bitmap("Imgs/nonca.png");
+	notaTatu->bitmap = al_load_bitmap("Imgs/ntatu.png");
+	notaJacare->bitmap = al_load_bitmap("Imgs/njacare.png");
+	notaMico->bitmap = al_load_bitmap("Imgs/nmico.png");
+
+	marcaOnca->bitmap = al_load_bitmap("Imgs/monca.png");
+	marcaTatu->bitmap = al_load_bitmap("Imgs/mtatu.png");
+	marcaJacare->bitmap = al_load_bitmap("Imgs/mjacare.png");
+	marcaMico->bitmap = al_load_bitmap("Imgs/mmico.png");
 
 	Background = al_load_bitmap("Imgs/fundo.png");
 
-	if (!mapa->bitmap || !Background)
-	{
-		fprintf(stderr, "Falha ao iniciar imagem\n");
-		al_destroy_display(janela);
-		return -1;
-	}
-
 	int gameOver = 0;
-
+	int arrastando = 0;
 	while (!gameOver)
 	{
 		while (!al_is_event_queue_empty(fila_eventos))
@@ -135,15 +139,89 @@ int JogarFase3Brasil(ALLEGRO_DISPLAY* janela, ALLEGRO_EVENT_QUEUE* fila_eventos,
 				prog->Gameover = 1;
 				gameOver = 1;
 			}
+			else if (evento.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
+				if (IsInside(evento.mouse.x, evento.mouse.y, marcaOnca))
+				{
+					arrastando = 1;
+					marcaOnca->cliqueX = MapearDistancia(evento.mouse.x, marcaOnca->x);
+					marcaOnca->cliqueY = MapearDistancia(evento.mouse.y, marcaOnca->y);
+				}
+				else if(IsInside(evento.mouse.x, evento.mouse.y, marcaTatu))
+				{
+					arrastando = 2;
+					marcaTatu->cliqueX = MapearDistancia(evento.mouse.x, marcaTatu->x);
+					marcaTatu->cliqueY = MapearDistancia(evento.mouse.y, marcaTatu->y);
+				}
+				else if (IsInside(evento.mouse.x, evento.mouse.y, marcaJacare))
+				{
+					arrastando = 3;
+					marcaJacare->cliqueX = MapearDistancia(evento.mouse.x, marcaJacare->x);
+					marcaJacare->cliqueY = MapearDistancia(evento.mouse.y, marcaJacare->y);
+				}
+				else if (IsInside(evento.mouse.x, evento.mouse.y, marcaMico))
+				{
+					arrastando = 4;
+					marcaMico->cliqueX = MapearDistancia(evento.mouse.x, marcaMico->x);
+					marcaMico->cliqueY = MapearDistancia(evento.mouse.y, marcaMico->y);
+				}
+				else {
+					arrastando = 0;
+				}
+			}
+			else if (evento.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP)
+			{
+				arrastando = 0;
+			}
+
+			ALLEGRO_MOUSE_STATE state;
+
+			al_get_mouse_state(&state);
+			if (state.buttons & 1 && arrastando)
+			{
+				switch (arrastando)
+				{
+				case 1:
+					if (!VerificarBordas(evento.mouse.x, evento.mouse.y, marcaOnca)) {
+						marcaOnca->x = evento.mouse.x - marcaOnca->cliqueX;
+						marcaOnca->y = evento.mouse.y - marcaOnca->cliqueY;
+					}
+					break;
+				case 2:
+					if (!VerificarBordas(evento.mouse.x, evento.mouse.y, marcaTatu)) {
+						marcaTatu->x = evento.mouse.x - marcaTatu->cliqueX;
+						marcaTatu->y = evento.mouse.y - marcaTatu->cliqueY;
+					}
+					break;
+				case 3:
+					if (!VerificarBordas(evento.mouse.x, evento.mouse.y, marcaJacare)) {
+						marcaJacare->x = evento.mouse.x - marcaJacare->cliqueX;
+						marcaJacare->y = evento.mouse.y - marcaJacare->cliqueY;
+					}
+					break;
+				case 4:
+					if (!VerificarBordas(evento.mouse.x, evento.mouse.y, marcaMico)) {
+						marcaMico->x = evento.mouse.x - marcaMico->cliqueX;
+						marcaMico->y = evento.mouse.y - marcaMico->cliqueY;
+					}
+					break;
+				default:
+					break;
+				}
+			}
 		}
 
 		al_draw_bitmap(Background, 0, 0, 0);
 		al_draw_bitmap(mapa->bitmap,mapa->x,mapa->y,0);
 
-		al_draw_bitmap(notaOnça->bitmap, notaOnça->x, notaOnça->y, 0);
+		al_draw_bitmap(notaOnca->bitmap, notaOnca->x, notaOnca->y, 0);
 		al_draw_bitmap(notaTatu->bitmap, notaTatu->x, notaTatu->y, 0);
 		al_draw_bitmap(notaJacare->bitmap, notaJacare->x, notaJacare->y, 0);
 		al_draw_bitmap(notaMico->bitmap, notaMico->x, notaMico->y, 0);
+
+		al_draw_bitmap(marcaOnca->bitmap, marcaOnca->x, marcaOnca->y, 0);
+		al_draw_bitmap(marcaTatu->bitmap, marcaTatu->x, marcaTatu->y, 0);
+		al_draw_bitmap(marcaJacare->bitmap, marcaJacare->x, marcaJacare->y, 0);
+		al_draw_bitmap(marcaMico->bitmap, marcaMico->x, marcaMico->y, 0);
 
 		al_flip_display();
 	}
--- AlhpaInkEscape/AlhpaInkEscape/Funcoes.h
+++ AlhpaInkEscape/AlhpaInkEscape/Funcoes.h
@@ -11,14 +11,9 @@
 #ifndef Funcoes_H
 #define Funcoes_H
 
-// Desenha um bitmap dado um Objeto
-void DesenharImagem(Objeto *objeto) {
-	al_draw_bitmap(objeto->bitmap, objeto->x, objeto->y, 0);
-}
-
 // Vereifica se as coordenadas (X,Y) estão dentro de uma imagem
-int IsInside(int x, int y, Objeto objeto) {
-	if (x >= objeto.x && x <= (objeto.x + objeto.largura) && y >= objeto.y && y <= (objeto.y + objeto.altura)) {
+int IsInside(int x, int y, Objeto *objeto) {
+	if (x >= objeto->x && x <= (objeto->x + objeto->largura) && y >= objeto->y && y <= (objeto->y + objeto->altura)) {
 		return 1;
 	}
 	return 0;
@@ -30,8 +25,8 @@ int MapearDistancia(int ponto, int c) {
 }
 
 // Verifica se uma imagem sendo arrastada ultrapassou as bordas da tela
-int VerificarBordas(int x, int y, Objeto imagem) {
-	if (x - imagem.cliqueX <= 0 || x - imagem.cliqueX + imagem.largura >= LARGURA_TELA || y - imagem.cliqueY <= 0 || y - imagem.cliqueY + imagem.altura >= ALTURA_TELA) {
+int VerificarBordas(int x, int y, Objeto *imagem) {
+	if (x - imagem->cliqueX <= 0 || x - imagem->cliqueX + imagem->largura >= LARGURA_TELA || y - imagem->cliqueY <= 0 || y - imagem->cliqueY + imagem->altura >= ALTURA_TELA) {
 		return 1;
 	}
 	return 0;
