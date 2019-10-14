f040cc10a6e03fdb16bc444d67318e4f2440f0a4
Igor Kenzo <36746677+IgorKenzo@users.noreply.github.com>
2019-10-14 15:29:19 -0300

    Revert "Merge tabperiodica c/ DesenvGustavo"
    This reverts commit 3b447b54d6737bb5c2e6ab365d7ee25314ad3329, reversing
    changes made to 579c40a1858faf1103227e2cedef1b7afcb3cf38.

--- AlphaInkEscape/AlhpaInkEscape/AlhpaInkEscape/FaseController.h
+++ AlphaInkEscape/AlhpaInkEscape/AlhpaInkEscape/FaseController.h
@@ -7,7 +7,6 @@
 #include <allegro5/allegro_native_dialog.h>
 
 #include "Struct.h"
-#include "tabelaPeri.h"
 #include "Fase4Binary.h"
 //#include "Fase0.h"
 //#include "Fase1.h"
@@ -22,9 +21,12 @@ void selecionaFase(ALLEGRO_DISPLAY* janela, ALLEGRO_EVENT_QUEUE* fila_eventos, P
 {
 	switch (progresso->proximaSala)
 	{
-	case 0: //JogarFase0(janela, fila_eventos, progresso);
+	case 0: 
+		//JogarFase0(janela, fila_eventos, progresso);
 		return;
-	case 1: JogarTabelaPeri(janela, fila_eventos, progresso);
+	case 1: //JogarFase1(janela, fila_eventos, progresso);
+		return;
+	case 2: //JogarFase2(janela, fila_eventos, progresso);
 		return;
 	case 3: //JogarFase3(janela, fila_eventos, progresso);
 		return;
