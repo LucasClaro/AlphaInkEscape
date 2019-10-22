e45caed49f9382bc3cfbc21c87428145a369b372
Igor Kenzo <36746677+IgorKenzo@users.noreply.github.com>
2019-10-22 15:20:50 -0300

    Update FaseController.h

--- AlhpaInkEscape/AlhpaInkEscape/FaseController.h
+++ AlhpaInkEscape/AlhpaInkEscape/FaseController.h
@@ -1,33 +1,39 @@
 #include <stdio.h>
-// Inclui o arquivo de cabeçalho da biblioteca Allegro 5
+// Inclui o arquivo de cabeï¿½alho da biblioteca Allegro 5
 #include <allegro5/allegro.h>
-// Inclui o cabeçalho do add-on para uso de imagens
+// Inclui o cabeï¿½alho do add-on para uso de imagens
 #include <allegro5/allegro_image.h>
 // Inclui a coisa do mouse
 #include <allegro5/allegro_native_dialog.h>
 
 #include "Struct.h"
-//#include "Fase0.h"
+#include "tabelaPeri.h"
+#include "Fase4Binary.h"
+#include "Fase0Conta.h"
 //#include "Fase1.h"
 //#include "Fase2.h"
 #include "Fase3Brasil.h"
-#include "FaseArco.h"
 
 #ifndef FaseController_H
 #define FaseController_H
 
-//Pega por referencia o lugar onde está guardado o progresso do jogo
+//Pega por referencia o lugar onde estï¿½ guardado o progresso do jogo
 void selecionaFase(ALLEGRO_DISPLAY* janela, ALLEGRO_EVENT_QUEUE* fila_eventos, Progresso* progresso)
 {
 	switch (progresso->proximaSala)
 	{
-	case 0: 
-		JogarFase3Brasil(janela, fila_eventos, progresso);
+	case 0: JogarFase0Conta(janela, fila_eventos, progresso);
 		return;
-	case 1: 
-		JogarFaseTeste(janela, fila_eventos, progresso);
+	case 1: JogarTabelaPeri(janela, fila_eventos, progresso);
 		return;
-	default: printf("Fase não encontrada"); return;
+	case 2: JogarFaseTeste(janela, fila_eventos,progresso);
+		return;
+	case 3: JogarFase3Brasil(janela, fila_eventos, progresso);
+		return;
+	case 4: 
+		JogarFase4Binary(janela, fila_eventos, progresso);
+		return;
+	default: printf("Fase nï¿½o encontrada"); return;
 		break;
 	}
 }
